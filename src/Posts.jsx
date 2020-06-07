fd2783240320b2b050cb7b80ef304ea8bc446f44
manjeet5 <manjeet175@gmail.com>
2020-06-07 12:06:48 -0400

    completed logic for post header

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -6,8 +6,37 @@ const DisplayPhoto = ({photoUrl}) => {
     </div>
 };
 
+const determineTimeDetails = (minutes) => {
+    const MINUTES_IN_HOUR = 60,
+    MINUTES_IN_DAY = 24 * MINUTES_IN_HOUR,
+    MINUTES_IN_WEEK = MINUTES_IN_DAY * 7,
+    MINUTES_IN_MONTH = MINUTES_IN_WEEK * 4,
+    MINUTES_IN_YEAR = MINUTES_IN_MONTH * 12;
+    if (minutes < MINUTES_IN_HOUR) {
+        return `${minutes} MINUTES AGO`;
+    } else if(minutes < MINUTES_IN_DAY) {
+        return `${Math.floor(minutes/MINUTES_IN_HOUR)} HOURS AGO`;
+    } else if(minutes < MINUTES_IN_WEEK) {
+        return `${Math.floor(minutes/MINUTES_IN_DAY)} DAYS AGO`;
+    } else if(minutes < MINUTES_IN_MONTH) {
+        return `${Math.floor(minutes/MINUTES_IN_WEEK)} WEEKS AGO`;
+    } else if(minutes < MINUTES_IN_YEAR) {
+        return `${Math.floor(minutes/MINUTES_IN_MONTH)} MONTH AGO`;
+    } else {
+        return `${Math.floor(minutes/MINUTES_IN_YEAR)} YEAR AGO`;
+    }
+}
 const PostHeader = ({name, userType, creationTime}) => {
-    return <div className="post-header">header goes here</div>
+    console.log(creationTime,  Date.now());
+    const elapsedTimeInMinutes = (Date.now() - creationTime)/(1000 * 60);
+    console.log("elapsedTime", elapsedTimeInMinutes);
+    return (
+        <div className="post-header">
+            <div className="post-header-name">{name}</div>
+            <div className="post-header-user-type">{userType}</div>
+            <div className="post-header-creation-time">{determineTimeDetails(elapsedTimeInMinutes)}</div>
+        </div>
+    );
 }
 
 const PostMessage =({message}) => {
